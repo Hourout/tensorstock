dd6cbf04185391dc44f1da562126c2669a72734e
JinQing Lee <hourout@163.com>
2019-01-29 14:04:25 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from io import open
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        return f.read()
+
+setup(name='tensorseries',
+      version='0.2.0',
+      install_requires=['tensordata>=0.0.1', 'tensorflow>=1.12.0'],
+      description='',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/Hourout/tensorseries',
+      author='JinQing Lee',
+      author_email='hourout@163.com',
+      keywords=['computer-vision', 'keras', 'tf.keras', 'deep-learning'],
+      license='Apache License Version 2.0',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          'Topic :: Scientific/Engineering :: Visualization',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6'
+      ],
+      packages=find_packages(),
+      zip_safe=False)
