03c93459454e8ef119e01d2241b8438ad2c738c8
JinQing Lee <hourout@163.com>
2019-02-02 15:46:37 +0800

    Update _regression.py

--- tensorseries/metrics/_regression.py
+++ tensorseries/metrics/_regression.py
@@ -3,11 +3,14 @@ import pandas as pd
 
 __all__ = ['normal_loss', 'l1loss', 'l2loss']
 
-def normal_loss(y_true, y_pred, k):
-    return np.power((y_true-y_pred).abs().pow(k).sum(), 1/k)
+def normal_loss(y_true, y_pred, k, log=False):
+    if log:
+        return np.power((np.log1p(y_true)-np.log1p(y_pred)).abs().pow(k).sum(), 1/k)
+    else:
+        return np.power((y_true-y_pred).abs().pow(k).sum(), 1/k)
 
-def l1loss(y_true, y_pred):
-    return normal_loss(y_true, y_pred, k=1)
+def l1loss(y_true, y_pred, log=False):
+    return normal_loss(y_true, y_pred, k=1, log=log)
 
 def l2loss(y_true, y_pred):
-    return normal_loss(y_true, y_pred, k=2)
+    return normal_loss(y_true, y_pred, k=2, log=log)
