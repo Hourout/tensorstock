658be947e8af753c102e5091c966b5507b430042
JinQing Lee <hourout@163.com>
2019-02-15 13:38:53 +0800

    Update _metrics.py

--- tensorseries/chart/_metrics.py
+++ tensorseries/chart/_metrics.py
@@ -4,12 +4,12 @@ import pyecharts as pe
 
 __all__ = ['ks_curve', 'roc_curve', 'pr_curve', 'lift_curve', 'gain_curve', 'gini_curve']
 
-def ks_curve(y_true, y_pred, label=1, jupyter=True, path='Kolmogorov-Smirnov Curve.html'):
+def ks_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Kolmogorov-Smirnov Curve.html'):
     t = pd.DataFrame({'prob':y_pred, 'label':y_true})
     assert t.label.nunique()==2, "`y_true` should be binary classification."
-    label_dict = {i:1 if i==label else 0 for i in t.label.unique()}
+    label_dict = {i:1 if i==pos_label else 0 for i in t.label.unique()}
     t['label'] = t.label.replace(label_dict)
-    t = t.sort_values(['prob'], ascending=False).reset_index(drop=True)
+    t = t.sort_values(['prob', 'label'], ascending=False).reset_index(drop=True)
     t['tp'] = t.label.cumsum()
     t['fp'] = t.index+1-t.tp
     t['tpr'] = t.tp/t.label.sum()
@@ -25,12 +25,12 @@ def ks_curve(y_true, y_pred, label=1, jupyter=True, path='Kolmogorov-Smirnov Cur
              is_smooth=True, mark_point=["max"], yaxis_type='value', xaxis_type='value')
     return line if jupyter else line.render(path)
 
-def roc_curve(y_true, y_pred, label=1, jupyter=True, path='Receiver Operating Characteristic Curve.html'):
+def roc_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Receiver Operating Characteristic Curve.html'):
     t = pd.DataFrame({'prob':y_pred, 'label':y_true})
     assert t.label.nunique()==2, "`y_true` should be binary classification."
-    label_dict = {i:1 if i==label else 0 for i in t.label.unique()}
+    label_dict = {i:1 if i==pos_label else 0 for i in t.label.unique()}
     t['label'] = t.label.replace(label_dict)
-    t = t.sort_values(['prob'], ascending=False).reset_index(drop=True)
+    t = t.sort_values(['prob', 'label'], ascending=False).reset_index(drop=True)
     t['tp'] = t.label.cumsum()
     t['fp'] = t.index+1-t.tp
     t['tpr'] = t.tp/t.label.sum()
@@ -43,12 +43,12 @@ def roc_curve(y_true, y_pred, label=1, jupyter=True, path='Receiver Operating Ch
              yaxis_type='value', xaxis_type='value', xaxis_name='FPR', yaxis_name='TPR')
     return line if jupyter else line.render(path)
 
-def pr_curve(y_true, y_pred, label=1, jupyter=True, path='Precision Recall Curve.html'):
+def pr_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Precision Recall Curve.html'):
     t = pd.DataFrame({'prob':y_pred, 'label':y_true})
     assert t.label.nunique()==2, "`y_true` should be binary classification."
-    label_dict = {i:1 if i==label else 0 for i in t.label.unique()}
+    label_dict = {i:1 if i==pos_label else 0 for i in t.label.unique()}
     t['label'] = t.label.replace(label_dict)
-    t = t.sort_values(['prob'], ascending=False).reset_index(drop=True)
+    t = t.sort_values(['prob', 'label'], ascending=False).reset_index(drop=True)
     t['tp'] = t.label.cumsum()
     t['fp'] = t.index+1-t.tp
     t['recall'] = t.tp/t.label.sum()
@@ -59,12 +59,12 @@ def pr_curve(y_true, y_pred, label=1, jupyter=True, path='Precision Recall Curve
              yaxis_type='value', xaxis_type='value', xaxis_name='recall', yaxis_name='precision')
     return line if jupyter else line.render(path)
 
-def lift_curve(y_true, y_pred, label=1, jupyter=True, path='Lift Curve.html'):
+def lift_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Lift Curve.html'):
     t = pd.DataFrame({'prob':y_pred, 'label':y_true})
     assert t.label.nunique()==2, "`y_true` should be binary classification."
-    label_dict = {i:1 if i==label else 0 for i in t.label.unique()}
+    label_dict = {i:1 if i==pos_label else 0 for i in t.label.unique()}
     t['label'] = t.label.replace(label_dict)
-    t = t.sort_values(['prob'], ascending=False).reset_index(drop=True)
+    t = t.sort_values(['prob', 'label'], ascending=False).reset_index(drop=True)
     t['tp'] = t.label.cumsum()
     t['fp'] = t.index+1-t.tp
     t['precision'] = t.tp/(t.tp+t.fp)
@@ -77,12 +77,12 @@ def lift_curve(y_true, y_pred, label=1, jupyter=True, path='Lift Curve.html'):
              yaxis_type='value', xaxis_type='value')
     return line if jupyter else line.render(path)
 
-def gain_curve(y_true, y_pred, label=1, jupyter=True, path='Gain Curve.html'):
+def gain_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Gain Curve.html'):
     t = pd.DataFrame({'prob':y_pred, 'label':y_true})
     assert t.label.nunique()==2, "`y_true` should be binary classification."
-    label_dict = {i:1 if i==label else 0 for i in t.label.unique()}
+    label_dict = {i:1 if i==pos_label else 0 for i in t.label.unique()}
     t['label'] = t.label.replace(label_dict)
-    t = t.sort_values(['prob'], ascending=False).reset_index(drop=True)
+    t = t.sort_values(['prob', 'label'], ascending=False).reset_index(drop=True)
     t['tp'] = t.label.cumsum()
     t['fp'] = t.index+1-t.tp
     t['precision'] = t.tp/(t.tp+t.fp)
@@ -93,10 +93,10 @@ def gain_curve(y_true, y_pred, label=1, jupyter=True, path='Gain Curve.html'):
              is_smooth=True, yaxis_type='value', xaxis_type='value')
     return line if jupyter else line.render(path)
 
-def gini_curve(y_true, y_pred, label=1, jupyter=True, path='Gini Curve.html'):
+def gini_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Gini Curve.html'):
     t = pd.DataFrame({'prob':y_pred, 'label':y_true})
     assert t.label.nunique()==2, "`y_true` should be binary classification."
-    label_dict = {i:1 if i==label else 0 for i in t.label.unique()}
+    label_dict = {i:1 if i==pos_label else 0 for i in t.label.unique()}
     t['label'] = t.label.replace(label_dict)
     t = t.sort_values(['prob', 'label']).reset_index(drop=True)
     t['cum'] = t.label.cumsum()/t.label.sum()
