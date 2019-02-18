86f321667bc5ff0a41a037f0487a5d9acf30be3c
JinQing Lee <hourout@163.com>
2019-02-18 22:17:21 +0800

    Update _bins.py

--- tensorseries/feature/bins/_bins.py
+++ tensorseries/feature/bins/_bins.py
@@ -22,20 +22,26 @@ def cut_box(feature, bins, one_hot=False, one_hot_name='columns'):
         t = pd.get_dummies(t, prefix=one_hot_name)
     return t
 
-def merge_chiSquare(df, index, merge_index):
-    df.loc[merge_index, 'fea_cnt'] = df.at[merge_index, 'fea_cnt'] + df.at[index, 'fea_cnt']
-    df.loc[merge_index, 'fea_sum'] = df.at[merge_index, 'fea_sum'] + df.at[index, 'fea_sum']
-    df.loc[merge_index, 'chi'] = (df.at[merge_index, 'fea_sum'] - df.at[merge_index, 'fea_cnt'])**2 /df.at[merge_index, 'fea_cnt']
-    df = df.drop([index]).reset_index(drop=True)
+def merge_chiSquare(df, merge_idx, idx):
+    df.loc[idx, 'actual_0'] = df.at[idx, 'actual_0'] + df.at[merge_idx, 'actual_0']
+    df.loc[idx, 'actual_1'] = df.at[idx, 'actual_1'] + df.at[merge_idx, 'actual_1']
+    df.loc[idx, 'expect_0'] = df.at[idx, 'expect_0'] + df.at[merge_idx, 'expect_0']
+    df.loc[idx, 'expect_1'] = df.at[idx, 'expect_1'] + df.at[merge_idx, 'expect_1']
+    chi2_0 = (df.at[idx, 'expect_0'] - df.at[idx, 'actual_0'])**2 / df.at[idx, 'expect_0']
+    chi2_1 = (df.at[idx, 'expect_1'] - df.at[idx, 'actual_1'])**2 / df.at[idx, 'expect_1']
+    df.at[idx, 'chi'] = chi2_0 + chi2_1
+    df = df.drop([merge_idx]).reset_index(drop=True)
     return df
 
 def chi_square_interval(feature, label, max_interval=5):
     t = pd.DataFrame({'label':label, 'feature':feature})
     rate = label.sum()/label.count()
     t = t.groupby(['feature']).label.agg(['count', 'sum']).reset_index()
-    t.columns = ['feature', 'fea_cnt', 'fea_sum']
-    t['fea_cnt'] = t.fea_cnt*rate
-    t['chi'] = np.square(t.fea_sum-t.fea_cnt)/t.fea_cnt
+    t.columns = ['feature', 'actual_all', 'actual_1']
+    t['actual_0'] = t.actual_all-t.actual_1
+    t['expect_1'] = t.actual_all*rate
+    t['expect_0'] = t.actual_all*(1-rate)
+    t['chi'] = np.square(t.expect_0 - t.actual_0)/t.expect_0+np.square(t.expect_1 - t.actual_1)/t.expect_1
     while(len(t) > max_interval):
         min_index = t[t.chi==t.chi.min()].index.tolist()[0]
         if min_index == 0:
@@ -51,13 +57,15 @@ def chi_square_interval(feature, label, max_interval=5):
     t, bins = pd.cut(feature, t.feature.values, labels=range(t.feature.count()-1), retbins=True)
     return t, bins
 
-def chi_square_threshold(feature, label, dfree=1, cf=0.1, min_interval=4):
+def chi_square_threshold(feature, label, dfree=4, cf=0.1, min_interval=4):
     t = pd.DataFrame({'label':label, 'feature':feature})
     rate = label.sum()/label.count()
     t = t.groupby(['feature']).label.agg(['count', 'sum']).reset_index()
-    t.columns = ['feature', 'fea_cnt', 'fea_sum']
-    t['fea_cnt'] = t.fea_cnt*rate
-    t['chi'] = np.square(t.fea_sum-t.fea_cnt)/t.fea_cnt
+    t.columns = ['feature', 'actual_all', 'actual_1']
+    t['actual_0'] = t.actual_all-t.actual_1
+    t['expect_1'] = t.actual_all*rate
+    t['expect_0'] = t.actual_all*(1-rate)
+    t['chi'] = np.square(t.expect_0 - t.actual_0)/t.expect_0+np.square(t.expect_1 - t.actual_1)/t.expect_1
     threshold = chi2.isf(cf, dfree)
     while(t.chi.min() < threshold and len(t) > min_interval):
         min_index = t[t.chi==t.chi.min()].index.tolist()[0]
@@ -71,6 +79,5 @@ def chi_square_threshold(feature, label, dfree=1, cf=0.1, min_interval=4):
             else:
                 t = merge_chiSquare(t, min_index-1, min_index)
     t.loc[t.feature==t.feature.min(), 'feature'] = t.feature.min()-0.0001
-    
     t, bins = pd.cut(feature, t.feature.values, labels=range(t.feature.count()-1), retbins=True)
     return t, bins
