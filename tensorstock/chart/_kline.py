186451a96953a6a8d2656b3e5ad45c3e4edfed3b
Hourout <hourout@163.com>
2021-08-04 16:55:15 +0800

    add

--- tensorstock/chart/_kline.py
+++ tensorstock/chart/_kline.py
@@ -4,23 +4,9 @@ from pyecharts.charts import Kline
 __all__ = ['KLine']
 
 class KLine():
-    def __init__(self):
-        pass
-    
-    def render_notebook(self):
-        return self.c.render_notebook()
-    
-    def render_html(self, path="kline.html"):
-        return self.c.render(path)
-        
-    def kline(self, x_data, y_data, name="kline", title="Kline"):
-        self.c = (
+    def __init__(self, title="Kline"):
+        self._c = (
             Kline()
-            .add_xaxis(x_data)
-            .add_yaxis(name, y_data, 
-#                        markline_opts=opts.MarkLineOpts(
-#             data=[opts.MarkLineItem(type_="max", value_dim="open")]),
-                      )
             .set_global_opts(
                 xaxis_opts=opts.AxisOpts(is_scale=True),
                 yaxis_opts=opts.AxisOpts(is_scale=True, 
@@ -28,8 +14,21 @@ class KLine():
                         is_show=True, areastyle_opts=opts.AreaStyleOpts(opacity=1)
                     ),
                 ),
-                datazoom_opts=[opts.DataZoomOpts()],
+                datazoom_opts=[opts.DataZoomOpts(range_start=40, range_end=100)],
                 title_opts=opts.TitleOpts(title=title),
             )
         )
+        
+    def render_notebook(self):
+        return self._c.render_notebook()
+    
+    def render_html(self, path="kline.html"):
+        return self._c.render(path)
+        
+    def add_xaxis(self, x_data):
+        self._c.add_xaxis(x_data)
+        return self
+    
+    def add_yaxis(self, y_data, name="kline"):
+        self._c.add_yaxis(name, y_data)
         return self
